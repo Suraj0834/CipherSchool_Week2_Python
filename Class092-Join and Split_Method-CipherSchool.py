db6aa6ea53bace7d869ad5ba13050cee3f8d902b
Suraj0834 <119146368+Suraj0834@users.noreply.github.com>
2022-12-12 21:20:01 +0530

    Add files via upload

---
+++ Class092-Join and Split_Method-CipherSchool.py
@@ -0,0 +1,12 @@
+# split method
+user_info='harshit 24'.split()
+print(user_info)
+
+name,age='suraj,18'.split(",")
+print(name)
+print(age)
+
+
+#joint method
+user_info=['suraj','18']
+print(','.join(user_info))
---
+++ Class120-Update-CipherSchool.py
@@ -0,0 +1,7 @@
+user_info= {
+    'name': 'suraj',
+    'age' : '18',
+}
+more_info={'name' : 'suraj' , 'state' : 'UP' , 'Hobbies' : ['coding' , 'reading']}
+user_info.update(more_info)
+print(user_info)
